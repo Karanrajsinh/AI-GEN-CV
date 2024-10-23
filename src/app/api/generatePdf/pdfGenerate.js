4c21585347e3cb824987ab5274f4a013b552af2d
Karanrajsinh <vaghelakaranraj54@gmail.com>
2024-10-23 18:16:39 +0530

    debugging

--- src/app/api/generatePdf/pdfGenerate.js
+++ src/app/api/generatePdf/pdfGenerate.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer"
 
 export const generatePdf = async (htmlContent, width, height) => {
   const browser = await puppeteer.launch({
-    executablePath: "chrome@130.0.6723.58 /vercel/path1/.cache/puppeteer/chrome/linux-130.0.6723.58/chrome-linux64/chrome",
+    // executablePath: "chrome@130.0.6723.58 /vercel/path1/.cache/puppeteer/chrome/linux-130.0.6723.58/chrome-linux64/chrome",
     args: ['--no-sandbox', '--disable-setuid-sandbox'], // Necessary for running Puppeteer on Vercel // Specify Chrome path for Vercel
     headless: true,  // Ensure it runs in headless mode
   });
